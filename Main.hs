42a2d56af47da264e6a10efbd2f28f31887ec39d
Markus Klinik <markus.klinik@gmx.de>
2011-07-09 11:15:52 +0200

    simplify

--- Main.hs
+++ Main.hs
@@ -1,26 +1,29 @@
 -- * A group of people makes a trip.
--- * Someone pays a bill for a subset of the group.
+-- * Repeatedly: someone pays a bill for a subset of the group.
 -- * How much does anyone owe to anyone else?
 
 module Main where
 
-import Data.List (nub)
+-- configuration: edit here
 
 data Person = Hans | Klaus | Erna | Elke
     deriving (Show, Eq)
+
+database =
+    [ PayedFor Hans 500 [Klaus]
+    , PayedFor Klaus 510 [Hans]
+    , PayedFor Hans 10 [Erna, Elke]
+    ]
+
+-- end of configuration: leave the rest as it is
 
 -- payer, amount, receivers
 data PayedFor = PayedFor Person Double [Person]
     deriving Show
 
+-- the first person owes an amount to the second person
 data Owes = Owes Person Double Person
     deriving Show
-
-database =
-    [ PayedFor Hans 500 [Hans, Klaus, Erna, Elke]
-    , PayedFor Klaus 50 [Erna, Elke]
-    , PayedFor Elke 10.50 [Hans]
-    ]
 
 -- turns a payment into a list of debts
 pays2owes :: PayedFor -> [Owes]
@@ -30,36 +33,30 @@ pays2owes (PayedFor payer amount receivers) =
 
 allDebts = concatMap pays2owes database
 
--- normalizeDebts :: [Owes] -> [Owes]
-normalizeDebts debts =
-    map flup [(filterPair (p1, p2) allDebts, filterPair (p2, p1) allDebts) | (p1, p2) <- pairings participants]
+-- Takes a debt x and a list of debts ds.
+-- For each debt in ds: check if the two persons match the two persons in x,
+-- and update x accordingly.
+-- Returns the updated debt with all debts of ds absorbed into x, and the rest
+-- list which contains no more debts between the two persons of x.
+processOne :: Owes -> [Owes] -> (Owes, [Owes])
+processOne x [] = (x, [])
+processOne (Owes personA amount personB) ((Owes pA am pB):ds) =
+         if( personA == pA && personB == pB ) then processOne (Owes personA (amount + am) personB) ds
+    else if( personA == pB && personB == pA ) then processOne (Owes personA (amount - am) personB) ds
+    else (updatedDebt, (Owes pA am pB):rest)
+        where (updatedDebt, rest) = processOne (Owes personA amount personB) ds
 
-flup :: ([Owes], [Owes]) -> [Owes]
-flup ([], xs) = sumDebts xs
-flup (xs, []) = sumDebts xs
-flup (xs, ys) =
-    if amountP1p2 > amountP2p1
-        then [Owes p1 (amountP1p2 - amountP2p1) p2]
-        else [Owes p2 (amountP2p1 - amountP1p2) p1]
-        where (Owes p1 amountP1p2 p2) = head $ sumDebts xs
-              (Owes _  amountP2p1 _ ) = head $ sumDebts ys
+processAll :: [Owes] -> [Owes]
+processAll [] = []
+processAll (d:ds) = updatedDebt : (processAll rest)
+    where (updatedDebt, rest) = processOne d ds
 
--- Generates all possible pairings from a list of participants
-pairings :: [a] -> [(a, a)]
-pairings [] = []
-pairings (x:xs) = [(x, y) | y <- xs] ++ (pairings xs)
+-- if the amount of a debt is negative, flip the two persons and invert the amount
+flipDebt :: Owes -> Owes
+flipDebt (Owes pA amount pB) =
+    if( amount < 0 )
+        then (Owes pB (-amount) pA)
+        else (Owes pA amount pB)
 
--- Filters a list of debts by the debts that p1 owes p2
-filterPair :: (Person, Person) -> [Owes] -> [Owes]
-filterPair (p1, p2) debts = filter (\(Owes x1 _ x2) -> p1 == x1 && p2 == x2) debts
-
--- Sums all debts of a list of debts.
--- Only works for filtered lists
-sumDebts [] = []
-sumDebts (x:[]) = [x]
-sumDebts ((Owes p1 a p2):(Owes _ b _):xs) = sumDebts ((Owes p1 (a + b) p2):xs)
-
--- Extracts the list of people who either payed or someone payed for them.
--- Only these persons show up in the final summary
-participants :: [Person]
-participants = nub $ concat [p:ps | (PayedFor p _ ps) <- database]
+main = do
+    putStrLn $ show $ map flipDebt $ processAll allDebts
